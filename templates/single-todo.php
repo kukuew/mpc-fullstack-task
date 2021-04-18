ac97fd64c34f9ed8e46ed97a3b3bb21a3a05d368
Mikołaj <mykola@360px.pl>
2021-04-18 13:20:24 +0200

    FIX – single-todo; UPDATE – css font-family;

--- templates/single-todo.php
+++ templates/single-todo.php
@@ -3,13 +3,23 @@
  * Single content
  */
 
-defined( 'ABSPATH' ) || exit;
+defined( 'ABSPATH' ) || exit; ?>
 
-wp_head(); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head(); ?>
+</head>
 
 <body <?php body_class(); ?>>
     <?php mpc_ft_get_template( 'content-todo' ); ?>
 </body>
 
-<?php
-wp_footer();
+<?php wp_footer(); ?>
+</html>
+
+
